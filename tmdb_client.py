690a438266ed3425a1e2306c1ab7420701f4b4fa
Anastasiia Sirenko <nastya9701@ukr.net>
2023-04-30 16:47:54 +0300

    movies library

---
+++ tmdb_client.py
@@ -0,0 +1,45 @@
+import requests
+
+api_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2NlNjVjZTA2OWUyMjA0ZmFhMzFmZmY2ZDNkYzVmZiIsInN1YiI6IjY0MmMzNGJmYzA0NDI5MDFmMDAzYmFkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1BwAwv6HS7rMSkH7DiyDCj1Hbs8jPQPF74cgLLXusBI'
+api_key = 'ccce65ce069e2204faa31fff6d3dc5ff'
+
+def get_popular_movies(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_movies(how_many, list_type):
+    data = get_popular_movies(list_type)
+    return data["results"][:how_many]
